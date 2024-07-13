b18269290e11fa60b53cc7a2fe846101516cc2a1
business-sixtick <business4dyd@gmail.com>
2024-07-13 05:09:19 +0000

    웹 에디터 테스트

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 print('hello')
 print('git hub에서 직접 수정 테스트')
+
+print('헐~ 깃용 웹 에디터가 있는데 비주얼코드랑 똑같이 생겼네???? 여기서 수정하면 어디에 저장 되는건가?')
+'인텔리센스도 동작하나?'
+import random
+test_dic = {key : value for key, value in enumerate(random.sample(range(60,100), 5)) }
+for i in test_dic.items() : # 튜플 형태로 주는구나
+    print(i)
+
+print('설마 웹에서 실행 되나?')
